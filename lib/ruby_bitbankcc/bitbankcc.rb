42a6333047dd56526ebbb64805a8768fae5a8af3
leyla <leyla@astralfield.blue>
2024-12-06 19:27:51 +0900

    feat: add request time

--- lib/ruby_bitbankcc/bitbankcc.rb
+++ lib/ruby_bitbankcc/bitbankcc.rb
@@ -10,6 +10,8 @@ require 'rest_client'
 class Bitbankcc
   @@base_url = "https://api.bitbank.cc"
   @@base_public_url = "https://public.bitbank.cc"
+  @@auth_method = "request_time"
+  @@time_window = 5000
   @@ssl = true
 
   def initialize(key = nil, secret = nil, params = {})
@@ -18,6 +20,12 @@ class Bitbankcc
     if !params[:base_url].nil?
       @@base_url = params[:base_url]
     end
+    if !params[:auth_method].nil?
+      @@auth_method = params[:auth_method]
+    end
+    if !params[:time_window].nil?
+      @@time_window = params[:time_window]
+    end
     if !params[:ssl].nil?
       @@ssl = params[:ssl]
     end
@@ -25,13 +33,13 @@ class Bitbankcc
 
   def read_balance
     path = "/v1/user/assets"
-    nonce = Time.now.to_i.to_s
+    nonce = get_current_milisec
     request_for_get(path, nonce)
   end
 
   def read_active_orders(pair, count = nil, from_id = nil, end_id = nil, since = nil, _end = nil)
     path = "/v1/user/spot/active_orders"
-    nonce = Time.now.to_i.to_s
+    nonce = get_current_milisec
     params = {
       pair: pair,
       count: count,
@@ -45,7 +53,7 @@ class Bitbankcc
 
   def create_order(pair, amount, price, side, type, post_only = false, trigger_price = nil)
     path = "/v1/user/spot/order"
-    nonce = Time.now.to_i.to_s
+    nonce = get_current_milisec
     body = {
       pair: pair,
       amount: amount,
@@ -60,7 +68,7 @@ class Bitbankcc
 
   def cancel_order(pair, order_id)
     path = "/v1/user/spot/cancel_order"
-    nonce = Time.now.to_i.to_s
+    nonce = get_current_milisec
     body = {
       pair: pair,
       order_id: order_id
@@ -70,7 +78,7 @@ class Bitbankcc
 
   def read_trade_history(pair, count = nil, order_id = nil, since = nil, _end = nil, order = nil)
     path = "/v1/user/spot/trade_history"
-    nonce = Time.now.to_i.to_s
+    nonce = get_current_milisec
     params = {
       pair: pair,
       count: count,
@@ -85,7 +93,7 @@ class Bitbankcc
 
   def read_deposit_history(asset, count = nil, since = nil, _end = nil, order = nil)
     path = "/v1/user/deposit_history"
-    nonce = Time.now.to_i.to_s
+    nonce = get_current_milisec
     params = {
       asset: asset,
       count: count,
@@ -99,7 +107,7 @@ class Bitbankcc
 
   def read_withdrawal_account(asset)
     path = "/v1/user/withdrawal_account"
-    nonce = Time.now.to_i.to_s
+    nonce = get_current_milisec
     params = {
       asset: asset
     }.compact
@@ -109,7 +117,7 @@ class Bitbankcc
 
   def request_withdrawal(asset, uuid, amount, otp_token = nil, sms_token = nil)
     path = "/v1/user/request_withdrawal"
-    nonce = Time.now.to_i.to_s
+    nonce = get_current_milisec
     body = {
       asset: asset,
       uuid: uuid,
@@ -122,7 +130,7 @@ class Bitbankcc
 
   def read_withdrawal_history(asset, count = nil, since = nil, _end = nil, order = nil)
     path = "/v1/user/withdrawal_history"
-    nonce = Time.now.to_i.to_s
+    nonce = get_current_milisec
     params = {
       asset: asset,
       count: count,
@@ -166,17 +174,41 @@ class Bitbankcc
       response.body
     end
 
-    def request_for_get(path, nonce, query = {})
-      uri = URI.parse @@base_url + path
-      signature = get_get_signature(path, @secret, nonce, query)
 
+    def make_nonce_header(message, api_key, secret_key, nonce)
+      signature = get_signature(secret_key, message)
       headers = {
         "Content-Type" => "application/json",
-        "ACCESS-KEY" => @key,
+        "ACCESS-KEY" => api_key,
         "ACCESS-NONCE" => nonce,
         "ACCESS-SIGNATURE" => signature
       }
+    end
 
+    def make_request_time_header(message, api_key, secret_key, request_time, time_window)
+      signature = get_signature(secret_key, message)
+      headers = {
+        "Content-Type" => "application/json",
+        "ACCESS-KEY" => api_key,
+        "ACCESS-REQUEST-TIME" => request_time,
+        "ACCESS-TIME-WINDOW" => time_window.to_s,
+        "ACCESS-SIGNATURE" => signature
+      }
+    end
+
+    def request_for_get(path, nonce, query = {})
+      uri = URI.parse @@base_url + path
+      query_string = query.present? ? '?' + query.to_query : ''
+      if @@auth_method == "request_time"
+        request_time = get_current_milisec
+        message = request_time + @@time_window.to_s + path + query_string
+        headers = make_request_time_header(message, @key, @secret, request_time, @@time_window)
+      else
+        nonce = get_current_milisec
+        headers = make_nonce_header(path,nonce)
+        message = nonce + path + query_string
+        headers = make_nonce_header(message, @key, @secret, nonce)
+      end
       uri.query = query.to_query
       request = Net::HTTP::Get.new(uri.request_uri, initheader = headers)
       http_request(uri, request)
@@ -184,15 +216,16 @@ class Bitbankcc
 
     def request_for_post(path, nonce, body)
       uri = URI.parse @@base_url + path
-      signature = get_post_signature(@secret, nonce, body)
-
-      headers = {
-        "Content-Type" => "application/json",
-        "ACCESS-KEY" => @key,
-        "ACCESS-NONCE" => nonce,
-        "ACCESS-SIGNATURE" => signature,
-        "ACCEPT" => "application/json"
-      }
+      if @@auth_method == "request_time"
+        request_time = get_current_milisec
+        message = request_time + @@time_window.to_s + body
+        headers = make_request_time_header(message, @key, @secret, request_time, @@time_window)
+      else
+        nonce = get_current_milisec
+        headers = make_nonce_header(path,nonce)
+        message = nonce + body
+        headers = make_nonce_header(message, @key, @secret, nonce)
+      end
 
       request = Net::HTTP::Post.new(uri.request_uri, initheader = headers)
       request.body = body
@@ -200,14 +233,11 @@ class Bitbankcc
       http_request(uri, request)
     end
 
-    def get_get_signature(path, secret_key, nonce, query = {})
-      query_string = query.present? ? '?' + query.to_query : ''
-      message = nonce + path + query_string
-      signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), secret_key, message)
+    def get_current_milisec
+      (Time.now.to_f * 1000.0).to_i.to_s
     end
 
-    def get_post_signature(secret_key, nonce, body = "")
-      message = nonce + body
+    def get_signature(secret_key, message)
       signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), secret_key, message)
     end
 end
--- ruby_bitbankcc.gemspec
+++ ruby_bitbankcc.gemspec
@@ -27,7 +27,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "activesupport"
   spec.add_dependency "rest-client"
-  spec.add_development_dependency "bundler", "~> 1.9"
+  spec.add_development_dependency "bundler", "~> 2.4"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
 end
